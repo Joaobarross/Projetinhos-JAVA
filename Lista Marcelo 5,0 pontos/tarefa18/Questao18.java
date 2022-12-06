688e13e6d4332f6c864213f5eb1a35ac51a4acd0
Joaobarross <113859633+Joaobarross@users.noreply.github.com>
2022-12-06 14:21:28 -0300

    Create Questao18.java

---
+++ Lista Marcelo 5,0 pontos/tarefa18/Questao18.java
@@ -0,0 +1,12 @@
+package tarefa18;
+
+public class Questao18 {
+	 public static void main(String[]args) {
+	        Proprietario parada= new Proprietario();	       
+	        parada.setNome("joao");
+	        parada.setRg("79.371.085-4");
+	        parada.setCpf("088.787.989-38");
+	        System.out.println("Nome:"+parada.getNome()+"\nCpf:"+parada.getCpf()+"\nRg:"+parada.getRg());
+	        
+	 }
+}
